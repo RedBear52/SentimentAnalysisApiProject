fbe3ed427ee20041cf2336e3e7c8bc9025b425c7
ryan-spearman <ryanspearman@gmail.com>
2022-07-27 20:59:50 -0500

    the experimenting continues...

--- public/modules/showClient.js
+++ public/modules/showClient.js
@@ -6,6 +6,21 @@ const showClient = (data) => {
         data.score_tag,
         data.sentence_list[0].text
     )
+    const displayCard = document.getElementById('card')
+    const dataPointList = document.getElementById('ul')
+    // const dataList = displayCard.append(dataPointList)
+    // dataList.innerHTML( 
+    // `
+    //     <li>${data.subjectivity}</li>
+    //     <li>${data.score_tag}</li>
+    //     <li>${data.sentence_list[0].text}</li>
+    // `
+    // ) 
+    
+    
+    // need to iterate over a data object to dynamically add the list
+    // SEE landing page NavBar functionality for guidance/inspiration
+
  }
  
  export { showClient }
